c5f7776e62c0f24145b18ad45e368811aca7ae9b
Chris Bowdon <c.bowdon@gmail.com>
2013-07-02 21:38:11 +0100

    Implemented quickselect.

--- src/sort.hs
+++ src/sort.hs
@@ -3,13 +3,15 @@ module KNN.Sort
 , quicksort' ) where
 
 quickselect :: Ord a => Int -> [a] -> [a]
-quickselect n ls = []
-{-
-	| n == n' = lss ++ gtr
-	| n < n' = quickselect n lss ++ gtr
-	| n > n' = lss ++ quickselect (n - n') gtr
-		where (n', lss, gtr) = quicksort' n ls
--}
+quickselect n ls
+	| n == n' 	= lss ++ eq ++ gtr
+	| n < n' 	= (quickselect n lss) ++ eq ++ gtr
+	| n < n''	= lss ++ eq ++ gtr
+	| otherwise	= lss ++ (quickselect (n - n') (eq ++ gtr))
+		where 
+			(lss, eq, gtr) = quicksort' n ls
+			n' = length lss
+			n'' = n' + length eq
 
 quicksort' :: Ord a => Int -> [a] -> ([a], [a], [a])
 quicksort' n ls = qiter (ls!!n) ls [] [] []
